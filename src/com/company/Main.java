d59954ddba81c047172dc227de2ac32c526c72ec
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-11-27 17:12:06 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        /*Please, enter the month number*/
+	   int num = 12;
+       switch (num)
+       {
+         case 1: System.out.println("Season - Winter!");
+             break;
+         case 2: System.out.println("Season - Winter!");
+             break;
+         case 3: System.out.println("Season - Spring!");
+             break;
+         case 4: System.out.println("Season - Spring!");
+             break;
+         case 5: System.out.println("Season - Spring!");
+             break;
+         case 6: System.out.println("Season - Summer!");
+             break;
+         case 7: System.out.println("Season - Summer!");
+             break;
+         case 8: System.out.println("Season - Summer!");
+             break;
+         case 9: System.out.println("Season - Autumn!");
+             break;
+         case 10: System.out.println("Season - Autumn!");
+             break;
+         case 11: System.out.println("Season - Autumn!");
+             break;
+         case  12: System.out.println("Season - Winter!");
+             break;
+         default: System.out.println("Value was entered incorrectly");
+               break;
+       }
+    }
+}
